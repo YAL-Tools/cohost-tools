a5d18ec501fa4d74394836c38bafbae729675ffc
YellowAfterlife <yellowafterlife@hotmail.com>
2024-09-30 19:19:35 +0300

    Updated the gallery generator

--- gallery-generator/script.js
+++ gallery-generator/script.js
@@ -1,66 +1,18 @@
 (() => {
 //
+const findId = (id) => document.getElementById(id);
 const _input = document.getElementById("in");
 const _output = document.getElementById("out");
 const _convert = document.getElementById("convert");
 const _useClass = document.getElementById("useClass");
+const _bg0 = document.getElementById("bg-0");
+const _bg1 = document.getElementById("bg-1");
+const _fg0 = document.getElementById("fg-0");
+const _fg1 = document.getElementById("fg-1");
 const _preview = document.getElementById("preview");
 const _outPreview = document.getElementById("outPreview");
-const styles = {
-	outer: [
-		"position:relative",
-		"overflow:hidden",
-	],
-	nav: [
-		"display:inline-block",
-		"z-index:100",
-		"width:40px",
-		"height:40px",
-		"line-height:40px",
-		"text-align:center",
-		"color:rgb(var(--color-text))",
-		"border-radius:0.5rem",
-	],
-	enabled: [
-		"background:rgb(var(--color-foreground-600))"
-	],
-	disabled: [
-		"background:rgb(var(--color-secondary-200))"
-	],
-	clickable: [
-		"user-select:none",
-		"cursor:pointer",
-		"pointer-events:all",
-	],
-	controls: [
-		"text-align:right",
-		"pointer-events:none",
-	],
-	next: [
-		"margin:5px calc((50% - 40px) - 5px) 5px 5px",
-	],
-	prev: [
-		"position:absolute",
-		"left:calc(50% - 50px)",
-		"margin:5px",
-	],
-	offsetWhenOpen: [
-		"width:50px",
-		"display:inline-block",
-	],
-	page: [
-		"background:white",
-		"position:absolute",
-		"inset:0",
-	],
-	ref: ["margin:0 auto"],
-	img: [
-		"margin:0 auto",
-		"pointer-events:all",
-	],
-};
 const clPrefix = "gal-";
-function getCSS() {
+function getCSS(styles) {
 	let css = [];
 	for (let name of Object.keys(styles)) {
 		css.push(`.${clPrefix + name} {`)
@@ -72,14 +24,91 @@ function getCSS() {
 	}
 	return css.join("\n");
 }
-(function() {
-	let css = getCSS();
+function syncCSS(styles) {
+	let css = getCSS(styles);
 	document.getElementById("css").value = css;
 	document.getElementById("preview-css").innerHTML = css;
-})();
+}
+
+// https://stackoverflow.com/a/5499821/5578773
+const escapeHTML_map = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;'
+};
+function escapeHTML_mapper(tag) {
+	return escapeHTML_map[tag] || tag;
+}
+function escapeHTML(snip) {
+	return snip.replace(/[&<>]/g, escapeHTML_mapper);
+}
 
 function proc() {
 	const inLines = _input.value.split("\n");
+	const lbPrev = escapeHTML(document.getElementById("lb-prev").value);
+	const lbNext = escapeHTML(document.getElementById("lb-next").value);
+	//
+	const bgPage = document.getElementById("bg-page").value;
+	const fgPage = document.getElementById("fg-page").value;
+	const styles = {
+		outer: [
+			"position:relative",
+			"overflow:hidden",
+			"background:" + bgPage,
+			"color:" + fgPage,
+		],
+		nav: [
+			"display:inline-block",
+			// "z-index:100",
+			"width:40px",
+			"height:40px",
+			"line-height:40px",
+			"text-align:center",
+			"border-radius:0.5rem",
+		],
+		enabled: [
+			"background:" + _bg1.value,
+			"color:" + _fg1.value,
+		],
+		disabled: [
+			"background:" + _bg0.value,
+			"color:" + _fg0.value,
+		],
+		clickable: [
+			"user-select:none",
+			"cursor:pointer",
+			"pointer-events:all",
+		],
+		controls: [
+			"text-align:right",
+			"pointer-events:none",
+		],
+		next: [
+			"margin:5px calc((50% - 40px) - 5px) 5px 5px",
+		],
+		prev: [
+			"position:absolute",
+			"left:calc(50% - 50px)",
+			"margin:5px",
+		],
+		offsetWhenOpen: [
+			"width:45px",
+			"display:inline-block",
+		],
+		page: [
+			"background:" + bgPage,
+			"color:" + fgPage,
+			"position:absolute",
+			"inset:0",
+		],
+		ref: ["margin:0 auto"],
+		img: [
+			"margin:0 auto",
+			"pointer-events:all",
+		],
+	};
+	syncCSS(styles);
+	//
 	const rxURL = /^http[s]:\/\/.+$/;
 	const rxTag = /^!\[(.*?)\]\((.*?)\)$/;
 	const snippets = [];
@@ -135,21 +164,18 @@ function proc() {
 			html = `<div ${cc("outer")}>
 ${snippet}
 <div ${cc("controls")}>
-<div ${cc("nav prev disabled")}>&lt;</div>
-<!--next-->
+<div ${cc("nav prev disabled")}>${lbPrev}</div><!--next-->
 </div></div>
 `;
 		} else html = html.replace("<!--next-->", `<details>
-<summary ${cc("nav next clickable enabled")}>&gt;</summary>
-<div ${cc("offsetWhenOpen")}></div>
+<summary ${cc("nav next clickable enabled")}>${lbNext}</summary><div ${cc("offsetWhenOpen")}></div>
 <div ${cc("page")}>
 ${snippet}
 <div ${cc("controls")}>
-<div ${cc("nav prev enabled")}>&lt;</div>
-<!--next-->
+<div ${cc("nav prev enabled")}>${lbPrev}</div><!--next-->
 </div></div></details>`);
 	}
-	html = html.replace("<!--next-->", `<div ${cc("nav next disabled")}>&gt;</div>`);
+	html = html.replace("<!--next-->", `<div ${cc("nav next disabled")}>${lbNext}</div>`);
 	_output.value = html;
 	if (_preview.checked) _outPreview.innerHTML = html;
 };
@@ -158,4 +184,20 @@ _convert.onclick = (e) => { proc() };
 _useClass.onchange = (e) => { proc() };
 _preview.onchange = (e) => { proc() };
 _convert.onclick();
+
+let roots = [document.querySelector("table#config")];
+findId("save-settings").onclick = () => {
+	let name = findId("preset-name").value;
+	let obj = window.yalTools.serialize(roots, {
+		resourceType: "https://yal.cc/tools/cohost/gallery-generator",
+		resourceVersion: "1.0",
+		"preset-name": name,
+	});
+	window.yalTools.saveJSON(obj, name + ".json");
+};
+let loadInput = findId("load-settings");
+let loadForm = findId("load-settings-form");
+loadInput.onchange = () => {
+	window.yalTools.loadJSON(loadInput, loadForm, roots, proc);
+};
 })();
